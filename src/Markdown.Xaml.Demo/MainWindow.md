fe9b7b69475158fe5e58d0f4841670fc93f3b47f
Bevan Arps <unrepentantgeek@outlook.com>
2013-04-01 16:07:37 +1300

    Added demo application

---
+++ src/Markdown.Xaml.Demo/MainWindow.md
@@ -0,0 +1,27 @@
+﻿# Markdown.Xaml #
+
+Markdown XAML is a port of the popular *MarkdownSharp* Markdown processor, but 
+with one very significant difference: Instead of rendering to a string 
+containing HTML, it renders to a FlowDocument suitable for embedding into a 
+WPF window or usercontrol.
+
+## Features ##
+
+MarkDown.Xaml has a number of convenient features
+
+* The engine itself is a single file, for easy inclusion in your own projects
+* Code for the engine is structured in the same manner as the original MarkdownSharp  
+If there are any bug fixes to the regular expressions in MarkdownSharp, merging those fixes in the Markdown.Xaml should be straightforward
+* Includes a `TextToFlowDocumentConverter` to make it easy to bind Markdown text
+
+## What is this Demo? ##
+
+This demo application shows MarkDown.Xaml in use - as you make changes to the 
+left pane, the rendered MarkDown will appear in the right pane.
+
+### Source ###
+
+Review the source for this demo to see how MarkDown.Xaml works in practice, how to use the TextToFlowDocumentConverter,
+and how to style the output to appear the way you desire.
+
+
---
+++ src/Markdown.Xaml.Demo/MainWindow.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Markdown.Demo
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_Loaded_1(object sender, RoutedEventArgs e)
+        {
+            editSource.Text = LoadSample();
+        }
+
+        private string LoadSample()
+        {
+            var subjectType = GetType();
+            var subjectAssembly = subjectType.Assembly;
+            using (Stream stream = subjectAssembly.GetManifestResourceStream(subjectType.FullName + ".md"))
+            {
+                if (stream == null)
+                {
+                    return String.Format("Could not find sample text *{0}*.md", subjectType.FullName);
+                }
+
+                using (StreamReader reader = new StreamReader(stream))
+                {
+                    return reader.ReadToEnd();
+                }
+            }
+        }
+
+    }
+}
