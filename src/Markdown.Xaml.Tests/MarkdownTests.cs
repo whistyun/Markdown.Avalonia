84bedce8da67320c5f1801969ba8cbc49c8db581
Whistyun <kanadische_goldrute@yahoo.co.jp>
2019-09-14 18:37:10 +0900

    Update *.approved.txt

--- src/Markdown.Xaml.Tests/MarkdownTests.cs
+++ src/Markdown.Xaml.Tests/MarkdownTests.cs
@@ -25,6 +25,17 @@ namespace Markdown.Xaml.Tests
 
         [Test]
         [Apartment(ApartmentState.STA)]
+        public void Transform_givenTest2_generatesExpectedResult()
+        {
+            var text = LoadText("Test1.md");
+            var markdown = new Markdown();
+            markdown.DisabledTag = true;
+            var result = markdown.Transform(text);
+            Approvals.Verify(AsXaml(result));
+        }
+
+        [Test]
+        [Apartment(ApartmentState.STA)]
         public void Transform_givenLists_generatesExpectedResult()
         {
             var text = LoadText("Lists.md");
@@ -35,9 +46,21 @@ namespace Markdown.Xaml.Tests
 
         [Test]
         [Apartment(ApartmentState.STA)]
-        public void Transform_givenTables_generatesExpectedResult() {
+        public void Transform_givenTables1_generatesExpectedResult()
+        {
             var text = LoadText("Tables.md");
             var markdown = new Markdown();
+            var result = markdown.Transform(text);
+            Approvals.Verify(AsXaml(result));
+        }
+
+        [Test]
+        [Apartment(ApartmentState.STA)]
+        public void Transform_givenTables2_generatesExpectedResult()
+        {
+            var text = LoadText("Tables.md");
+            var markdown = new Markdown();
+            markdown.DisabledTag = true;
             var result = markdown.Transform(text);
             Approvals.Verify(AsXaml(result));
         }
@@ -54,10 +77,21 @@ namespace Markdown.Xaml.Tests
 
         [Test]
         [Apartment(ApartmentState.STA)]
-        public void Transform_givenLinksInline_generatesExpectedResult()
+        public void Transform_givenLinksInline1_generatesExpectedResult()
         {
             var text = LoadText("Links_inline_style.md");
             var markdown = new Markdown();
+            var result = markdown.Transform(text);
+            Approvals.Verify(AsXaml(result));
+        }
+
+        [Test]
+        [Apartment(ApartmentState.STA)]
+        public void Transform_givenLinksInline2_generatesExpectedResult()
+        {
+            var text = LoadText("Links_inline_style.md");
+            var markdown = new Markdown();
+            markdown.DisabledTootip = true;
             var result = markdown.Transform(text);
             Approvals.Verify(AsXaml(result));
         }
@@ -74,10 +108,36 @@ namespace Markdown.Xaml.Tests
 
         [Test]
         [Apartment(ApartmentState.STA)]
-        public void Transform_givenImages_generatesExpectedResult()
+        public void Transform_givenImages1_generatesExpectedResult()
         {
             var text = LoadText("Images.md");
             var markdown = new Markdown();
+            markdown.AssetPathRoot = Path.GetDirectoryName(Assembly.GetCallingAssembly().Location);
+
+            var result = markdown.Transform(text);
+            Approvals.Verify(AsXaml(result));
+        }
+
+        [Test]
+        [Apartment(ApartmentState.STA)]
+        public void Transform_givenImages2_generatesExpectedResult()
+        {
+            var text = LoadText("Images.md");
+            var markdown = new Markdown();
+            markdown.DisabledLazyLoad = true;
+            markdown.AssetPathRoot = Path.GetDirectoryName(Assembly.GetCallingAssembly().Location);
+
+            var result = markdown.Transform(text);
+            Approvals.Verify(AsXaml(result));
+        }
+
+        [Test]
+        [Apartment(ApartmentState.STA)]
+        public void Transform_givenImages3_generatesExpectedResult()
+        {
+            var text = LoadText("Images.md");
+            var markdown = new Markdown();
+            markdown.DisabledTootip = true;
             markdown.AssetPathRoot = Path.GetDirectoryName(Assembly.GetCallingAssembly().Location);
 
             var result = markdown.Transform(text);
