4b47991785ce0440bc107569992abcb892e0f0d4
whistyun <kanadische_goldrute@yahoo.co.jp>
2020-12-03 22:32:32 +0900

    add sample

---
+++ example/HowToUse/HowToUse/UseBinding.axaml.cs
@@ -0,0 +1,19 @@
+﻿using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace HowToUse
+{
+    public class UseBinding : UserControl
+    {
+        public UseBinding()
+        {
+            this.InitializeComponent();
+        }
+
+        private void InitializeComponent()
+        {
+            AvaloniaXamlLoader.Load(this);
+        }
+    }
+}
---
+++ example/HowToUse/HowToUse/UseBindingViewModel.cs
@@ -0,0 +1,23 @@
+﻿using ReactiveUI;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HowToUse
+{
+    public class UseBindingViewModel : ReactiveObject
+    {
+        private string _text;
+        public string Text
+        {
+            get => _text;
+            set => this.RaiseAndSetIfChanged(ref _text, value);
+        }
+
+
+        public UseBindingViewModel()
+        {
+            Text = "# Heading1\n\nHello markdown.\n\n* listitem1\n* listitem2\n\n| col1 | col2 | col3 |\n|------|------|------|\n| one  |------|------|\n| two  |------|------|\n\n> p>. and enhance syntax";
+        }
+    }
+}
