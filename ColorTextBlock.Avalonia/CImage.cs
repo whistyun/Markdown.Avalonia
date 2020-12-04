c512f87af300a4d556bfd07efa366128b343e79e
whistyun <kanadische_goldrute@yahoo.co.jp>
2020-12-05 01:28:46 +0900

    expose inlines of CTextBlock for styling.

--- ColorTextBlock.Avalonia/CImage.cs
+++ ColorTextBlock.Avalonia/CImage.cs
@@ -1,17 +1,49 @@
-﻿using Avalonia.Media;
+﻿using Avalonia;
+using Avalonia.Media;
 using Avalonia.Media.Imaging;
 using ColorTextBlock.Avalonia.Geometries;
+using System;
 using System.Collections.Generic;
+using System.Threading.Tasks;
 
 namespace ColorTextBlock.Avalonia
 {
     public class CImage : CInline
     {
+        public static readonly StyledProperty<double?> LayoutWidthProperty =
+            AvaloniaProperty.Register<CImage, double?>(nameof(LayoutWidth));
 
-        public Bitmap Image { get; }
+        public static readonly StyledProperty<double?> LayoutHeightProperty =
+            AvaloniaProperty.Register<CImage, double?>(nameof(LayoutHeight));
+
+        public double? LayoutWidth
+        {
+            get { return GetValue(LayoutWidthProperty); }
+            set { SetValue(LayoutWidthProperty, value); }
+        }
+        public double? LayoutHeight
+        {
+            get { return GetValue(LayoutHeightProperty); }
+            set { SetValue(LayoutHeightProperty, value); }
+        }
+
+        public Task<Bitmap> Task { get; }
+        private Bitmap WhenError { get; }
+        public Bitmap Image { private set; get; }
+
+        public CImage(Task<Bitmap> task, Bitmap whenError)
+        {
+            if (task is null) throw new NullReferenceException(nameof(task));
+            if (whenError is null) throw new NullReferenceException(nameof(whenError));
+
+            this.Task = task;
+            this.WhenError = whenError;
+        }
 
         public CImage(Bitmap bitmap)
         {
+            if (bitmap is null) throw new NullReferenceException(nameof(bitmap));
+
             this.Image = bitmap;
         }
 
@@ -21,7 +53,16 @@ namespace ColorTextBlock.Avalonia
             bool parentUnderline, bool parentStrikethough,
             double entireWidth, double remainWidth)
         {
-            yield return new BitmapGeometry(Image);
+            if (Image is null)
+            {
+                Task.Wait();
+                Image = Task.IsFaulted ? WhenError : Task.Result ?? WhenError;
+            }
+
+            yield return new BitmapGeometry(
+                Image,
+                LayoutWidth.HasValue ? LayoutWidth.Value : Image.Size.Width,
+                LayoutHeight.HasValue ? LayoutHeight.Value : Image.Size.Height);
         }
     }
 }
