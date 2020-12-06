d18ba8b4e828df52986e3446d90e5a9af2449cd7
whistyun <kanadische_goldrute@yahoo.co.jp>
2020-12-07 02:02:18 +0900

    allow to custom bitmaploader and hyperlinkcommand

---
+++ Markdown.Avalonia/Utils/IImageLoader.cs
@@ -0,0 +1,15 @@
+﻿using Avalonia.Media.Imaging;
+using Avalonia.Platform;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Markdown.Avalonia.Utils
+{
+    public interface IBitmapLoader
+    {
+        string AssetPathRoot { set; }
+
+        Bitmap Get(string urlTxt);
+    }
+}
