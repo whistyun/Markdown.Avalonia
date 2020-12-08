7eed0d26a662305df6011ea4c7e5be333024858b
whistyun <kanadische_goldrute@yahoo.co.jp>
2020-12-09 00:43:34 +0900

    refact code

--- Markdown.Avalonia/TextMarkerStyle.cs
+++ Markdown.Avalonia/TextMarkerStyle.cs
@@ -6,8 +6,6 @@ namespace Markdown.Avalonia
 {
     public enum TextMarkerStyle
     {
-        None,
-
         Box,
         Circle,
         Decimal,
@@ -20,5 +18,44 @@ namespace Markdown.Avalonia
         UpperRoman,
 
         Square,
+    }
+
+    public static class MarkdownStyleExt
+    {
+        public static string CreateMakerText(this TextMarkerStyle textMarker, int index)
+        {
+            switch (textMarker)
+            {
+                default:
+                    throw new InvalidOperationException("sorry library manager forget to modify about listmerker.");
+
+                case TextMarkerStyle.Disc:
+                    return "•";
+
+                case TextMarkerStyle.Box:
+                    return "▪";
+
+                case TextMarkerStyle.Circle:
+                    return "○";
+
+                case TextMarkerStyle.Square:
+                    return "❏";
+
+                case TextMarkerStyle.Decimal:
+                    return (index + 1).ToString() + ".";
+
+                case TextMarkerStyle.LowerLatin:
+                    return NumberToOrder.ToLatin(index + 1).ToLower() + ".";
+
+                case TextMarkerStyle.UpperLatin:
+                    return NumberToOrder.ToLatin(index + 1) + ".";
+
+                case TextMarkerStyle.LowerRoman:
+                    return NumberToOrder.ToRoman(index + 1).ToLower() + ".";
+
+                case TextMarkerStyle.UpperRoman:
+                    return NumberToOrder.ToRoman(index + 1) + ".";
+            }
+        }
     }
 }
