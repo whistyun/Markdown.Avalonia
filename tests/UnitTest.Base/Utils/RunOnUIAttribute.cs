12ba82bd19c6a51a8da597165ce0a3e33c138eef
whistyun <kanadische_goldrute@yahoo.co.jp>
2020-12-29 22:25:26 +0900

    Restructure test project

---
+++ tests/UnitTest.Base/Utils/RunOnUIAttribute.cs
@@ -0,0 +1,33 @@
+﻿using Avalonia.Threading;
+using NUnit.Framework.Interfaces;
+using NUnit.Framework.Internal;
+using NUnit.Framework.Internal.Commands;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UnitTest.Base.Utils
+{
+    [System.AttributeUsage(AttributeTargets.Method, Inherited = true, AllowMultiple = false)]
+    public sealed class RunOnUIAttribute : Attribute, IWrapTestMethod
+    {
+        public TestCommand Wrap(TestCommand command) => new RunOnUICommand(command);
+
+        class RunOnUICommand : DelegatingTestCommand
+        {
+            public RunOnUICommand(TestCommand innerCommand)
+                : base(innerCommand)
+            {
+            }
+
+            public override TestResult Execute(TestExecutionContext context)
+            {
+                var resultTask = Dispatcher.UIThread.InvokeAsync<TestResult>(() => innerCommand.Execute(context));
+
+                resultTask.Wait();
+
+                return resultTask.Result;
+            }
+        }
+    }
+}
